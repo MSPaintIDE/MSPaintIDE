f9b84530ebd691ba31f41ed4d98b13d099f1561c
RubbaBoy <adamyarris@yahoo.com>
2018-11-05 19:16:50 -0500

    Starting to add Python language, need to implement NewOCR first

---
+++ src/main/java/com/uddernetworks/mspaint/languages/python/MultiLineBlock.java
@@ -0,0 +1,67 @@
+package com.uddernetworks.mspaint.languages.python;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MultiLineBlock {
+    private List<String> lines;
+    private int length;
+    private int currentCharacter = -1;
+    private int currentLine = 0;
+
+    public MultiLineBlock(String string) {
+        this.lines = Arrays.stream(string.split("\n")).collect(Collectors.toList());
+        this.length = string.replace("\n", "").length();
+    }
+
+    public char getCharAt(int index) {
+        final int originalCurrentChar = currentCharacter;
+        final int originalCurrentLine = currentLine;
+
+        currentCharacter = -1;
+        currentLine = 0;
+
+        char result = 0;
+
+        int i = 0;
+        while (canContinue()) {
+            if (i++ == index) {
+                result = getNext();
+                break;
+            }
+        }
+
+        currentCharacter = originalCurrentChar;
+        currentLine = originalCurrentLine;
+
+        return result;
+    }
+
+    public char getNext() {
+        return lines.get(currentLine).charAt(currentCharacter);
+    }
+
+    public boolean canContinue() {
+        if (lines.get(currentLine).length() <= ++currentCharacter) {
+            currentCharacter = 0;
+            currentLine++;
+        } else {
+            return true;
+        }
+
+        return lines.size() > currentLine;
+    }
+
+    public int length(){
+        return this.length;
+    }
+
+    public int getCharacter() {
+        return this.currentCharacter;
+    }
+
+    public int getLine() {
+        return this.currentLine;
+    }
+}
